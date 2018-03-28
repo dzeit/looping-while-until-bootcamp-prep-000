f7dc0112e853a0e49a4088d2a0bf36a618abe8f1
dzeit216@gmail.com Zeit <dzeit216@gmail.com>
2018-03-28 14:26:11 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,8 @@
 def using_until
         levitation_force = 6
-        loop do 
         until levitation_force < 10 
         puts "Wingarium Leviosa"
         levitation_force += 1 
-        break
     end 
 end
 
